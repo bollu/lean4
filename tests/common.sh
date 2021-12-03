b96996b5a7f67a929a61972d85126f2db4edfceb
Siddharth Bhat <siddu.druid@gmail.com>
2021-12-03 18:37:51 +0530

    add ignore

--- tests/common.sh
+++ tests/common.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -euo pipefail
 
 ulimit -s 8192
@@ -21,19 +22,22 @@ shift
 [ $# -eq 0 ] || fail "Usage: test_single.sh [-i] test-file.lean"
 
 
-function compile_lean {
+
+LEAN4_PATH=/home/bollu/work/lean4
+
+function compile_lean {                  
     # lean --c="$f.c" "$f" || fail "Failed to compile $f into C file"
     # leanc -O3 -DNDEBUG -o "$f.out" "$@" "$f.c" || fail "Failed to compile C file $f.c"
     lean -m "$f.mlir" "$f"
     hask-opt "$f.mlir" --convert-scf-to-std --lean-lower --ptr-lower | \
       mlir-translate --mlir-to-llvmir -o "$f.ll"
-    llvm-link "$f.ll" /code/lz/lean-linking-incantations/lib-includes/library.ll -S | opt -O3 -S  | \
+    llvm-link "$f.ll" $LEAN4_PATH/lz/lean-linking-incantations/lib-includes/library.ll -S | opt -O3 -S  | \
     llc -O3 -march=x86-64 -filetype=obj  -o "$f.o"
-    g++ -D LEAN_MULTI_THREAD -I/code/lean4/build/release/stage1/include \
+    g++ -D LEAN_MULTI_THREAD -I$LEAN4_PATH/build/release/stage1/include \
             "$f.o" \
-            /code/lz/lean-linking-incantations/lean-shell.o \
+            $LEAN4_PATH/lz//lean-linking-incantations/lean-shell.o \
             -no-pie -Wl,--start-group -lleancpp -lInit -lStd -lLean -Wl,--end-group \
-            -L/code/lean4/build/release/stage1/lib/lean -lgmp -ldl -pthread \
+            -L$LEAN4_PATH/build/release/stage1/lib/lean -lgmp -ldl -pthread \
             -Wno-unused-command-line-argument -o "$@" "$f.out"
 }
 
