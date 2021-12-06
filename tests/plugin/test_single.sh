e4eed60577b675a3d0929f24837a7307692eef23
Siddharth Bhat <siddu.druid@gmail.com>
2021-12-06 13:46:31 +0530

    add PIC stuff to get plugin working

--- tests/plugin/test_single.sh
+++ tests/plugin/test_single.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 source ../common.sh
 
-compile_lean -shared -o "${f%.lean}.so"
+lean -m "$f.mlir" "$f"
+hask-opt "$f.mlir" --convert-scf-to-std --lean-lower --ptr-lower | \
+  mlir-translate --mlir-to-llvmir -o "$f.ll"
+llvm-link "$f.ll" /code/lz/lean-linking-incantations/lib-includes/library.ll -S | opt -O3 -S  | \
+llc -O3 -march=x86-64 -filetype=obj  -o "$f.o"
+g++ -D LEAN_MULTI_THREAD -I/code/lean4/build/release/stage1/include \
+        "$f.o" \
+        /code/lz/lean-linking-incantations/lean-shell.o \
+        -no-pie -Wl,--start-group -lleancpp -lInit -lStd -lLean -Wl,--end-group \
+        -L/code/lean4/build/release/stage1/lib/lean -lgmp -ldl -pthread \
+        -Wno-unused-command-line-argument -o -fPIC -shared "${f%.lean}.so"
+# compile_lean -shared -o "${f%.lean}.so"
 expected_ret=1
 exec_check lean --plugin="${f%.lean}.so" "$f"
 diff_produced
