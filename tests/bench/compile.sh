bba0635800651b02805d7a8801d50601504d58e6
Siddharth Bhat <siddu.druid@gmail.com>
2022-08-10 17:17:29 +0100

    wip: checking on full test suite
    ----
    CTEST_PARALLEL_LEVEL=40 ctest -R "(leancomptest|leanbenchtest).*"
    
    42% tests passed, 33 tests failed out of 57
    
    Total Test time (real) =   9.85 sec
    
    The following tests FAILED:
    	1334 - leancomptest_array_test.lean (Failed)
    	1335 - leancomptest_array_test2.lean (Failed)
    	1337 - leancomptest_binomial.lean (Failed)
    	1339 - leancomptest_closure_bug1.lean (Failed)
    	1340 - leancomptest_closure_bug2.lean (Failed)
    	1341 - leancomptest_closure_bug3.lean (Failed)
    	1347 - leancomptest_computedFieldsExtern.lean (Failed)
    	1349 - leancomptest_expr.lean (Failed)
    	1351 - leancomptest_float.lean (Failed)
    	1353 - leancomptest_init.lean (Failed)
    	1354 - leancomptest_lazylist.lean (Failed)
    	1355 - leancomptest_map_big.lean (Failed)
    	1360 - leancomptest_phashmap.lean (Failed)
    	1361 - leancomptest_phashmap2.lean (Failed)
    	1362 - leancomptest_phashmap3.lean (Failed)
    	1363 - leancomptest_print_error.lean (Failed)
    	1364 - leancomptest_qsortBadLt.lean (Failed)
    	1365 - leancomptest_rbmap_library.lean (Failed)
    	1366 - leancomptest_reusebug.lean (Failed)
    	1367 - leancomptest_str.lean (Failed)
    	1370 - leancomptest_t1.lean (Failed)
    	1371 - leancomptest_t2.lean (Failed)
    	1372 - leancomptest_t4.lean (Failed)
    	1373 - leancomptest_thunk.lean (Failed)
    	1374 - leancomptest_uint_fold.lean (Failed)
    	1418 - leanbenchtest_binarytrees.lean (Failed)
    	1419 - leanbenchtest_const_fold.lean (Failed)
    	1420 - leanbenchtest_deriv.lean (Failed)
    	1421 - leanbenchtest_liasolver.lean (Failed)
    	1422 - leanbenchtest_qsort.lean (Failed)
    	1423 - leanbenchtest_rbmap.lean (Failed)
    	1424 - leanbenchtest_rbmap_checkpoint.lean (Failed)
    	1425 - leanbenchtest_unionfind.lean (Failed)
    ----

--- tests/bench/compile.sh
+++ tests/bench/compile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source ../common.sh
 
+# TODO(bollu): what should this be? C or LLVM?
 compile_lean_c_backend
--- tests/bench/test_single.sh
+++ tests/bench/test_single.sh
@@ -4,3 +4,7 @@ source ../common.sh
 compile_lean_c_backend
 exec_check "./$f.out" $(cat "$f.args")
 diff_produced
+
+compile_lean_llvm_backend
+exec_check "./$f.out" $(cat "$f.args")
+diff_produced
